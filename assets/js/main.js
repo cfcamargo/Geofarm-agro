6ae51c4ab70ea067b5eca93eb82b509cf1b3d938
Cristian Camargo <chris.camargo2015@gmail.com>
2022-01-18 13:29:41 -0400

    Criando section about

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,7 @@ const menuNavigation = document.querySelector('#menu-navigation')
 const mobileButtonOpen = document.querySelector(".mobal-icon-open")
 const mobileButtonClose = document.querySelector(".mobal-icon-close")
 
+
 mobileButtonOpen.addEventListener('click', ()=>{
     menuNavigation.classList.add('show')
     setTimeout(()=>{
@@ -22,3 +23,32 @@ for(const link of links){
         mobileButtonClose.classList.remove('show')
     })
 };
+
+
+
+// adicionando efeito scrool
+const header = document.querySelector('#header');
+const navHeight = header.offsetHeight;
+const menuLogo = document.querySelector('#geo-logo')
+function changeHeaderOnScroll() {
+
+    if (window.scrollY >= navHeight){
+        // scroll e maior q a altura do header
+        header.classList.add('scroll')
+        menuLogo.setAttribute('src', './assets/images/logos/geo_blue.png')
+
+    } else {
+        // scroll e menor q a altura do header
+        header.classList.remove('scroll')
+        menuLogo.setAttribute('src', './assets/images/logos/geo_white.png')
+
+
+    }
+}
+
+
+
+
+window.addEventListener('scroll', () => {
+    changeHeaderOnScroll()
+});
